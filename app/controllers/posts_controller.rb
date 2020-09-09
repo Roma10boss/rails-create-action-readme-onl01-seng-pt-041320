43f6f726c939f83039c26d384320d21ab6b6e325
Romario Gustave <romagustave10@gmail.com>
2020-09-09 05:21:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,11 +12,11 @@ class PostsController < ApplicationController
   end
 
   # add create method here
-  def create 
+  def create
     @post = Post.new
     @post.title = params[:title]
     @post.description = params[:description]
-    @post.save 
+    @post.save
     redirect_to post_path(post)
-  end 
+  end
 end
